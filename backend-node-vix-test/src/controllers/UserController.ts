308b922dbdc58b0ed49e5e6741a7e16c7a78e893
jvsmatheus <matheusjvs_@hotmail.com>
2026-01-11 00:06:30 -0300

    refactor: renomeando variáveis

--- backend-node-vix-test/src/controllers/UserController.ts
+++ backend-node-vix-test/src/controllers/UserController.ts
@@ -5,31 +5,28 @@ import { CustomRequest } from "../types/custom";
 
 export class UserController {
   constructor() {}
-  private vMService = new UserService();
+  private userService = new UserService();
 
   async getById(req: CustomRequest<unknown>, res: Response) {
     const { idUser } = req.params;
-    const result = await this.vMService.getById(idUser);
+    const result = await this.userService.getById(idUser);
     return res.status(STATUS_CODE.OK).json(result);
   }
 
   async createUser(req: CustomRequest<unknown>, res: Response) {
-    // const user = req.user as user;
-    const result = await this.vMService.createNewUser(req.body);
+    const result = await this.userService.createNewUser(req.body);
     return res.status(STATUS_CODE.CREATED).json(result);
   }
 
   async updateUser(req: CustomRequest<unknown>, res: Response) {
     const { idUser } = req.params;
-    // const user = req.user as user;
-    const result = await this.vMService.updateUser(idUser, req.body);
+    const result = await this.userService.updateUser(idUser, req.body);
     return res.status(STATUS_CODE.OK).json(result);
   }
 
   async deleteUser(req: CustomRequest<unknown>, res: Response) {
     const { idUser } = req.params;
-    // const user = req.user as user;
-    const result = await this.vMService.deleteUser(idUser);
+    const result = await this.userService.deleteUser(idUser);
     return res.status(STATUS_CODE.OK).json(result);
   }
 }
